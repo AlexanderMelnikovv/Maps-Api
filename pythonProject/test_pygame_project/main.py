555430a1e2a6e0dd8ce789bdc691eabb144288da
Alexander Melnikov <Msan90546@mail.ru>
2022-12-24 22:30:36 +0300

    add a hero

--- pythonProject/test_pygame_project/main.py
+++ pythonProject/test_pygame_project/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 
 
 def terminate():
@@ -11,13 +12,48 @@ pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-rect = pygame.Rect(400, 400, 30, 30)
+rect_hero = pygame.Rect(400, 400, 30, 30)
 speed = 10
 FPS = 120
 jump = False
 jumpCount = 0
 jumpMax = 15
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с рисунком "{fullname}" не найден!')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is None:
+        image.convert_alpha()
+    else:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+class MainHero(pygame.sprite.Sprite):
+    image = load_image("mar.png")
+
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        self.image = MainHero.image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+
+all_sprites = pygame.sprite.Group()
+main_hero = MainHero(300, 500)
+all_sprites.add(main_hero)
 running = True
 while running:
     clock.tick(FPS)
@@ -28,18 +64,19 @@ while running:
             if not jump and event.key == pygame.K_SPACE:
                 jump = True
                 jumpCount = jumpMax
-
     pressed_keys = pygame.key.get_pressed()
-    rect.centerx = (rect.centerx + (pressed_keys[pygame.K_d] -
-                                      pressed_keys[pygame.K_a]) * speed) % 800
+    rect_hero.centerx = (rect_hero.centerx + (pressed_keys[pygame.K_d] -
+                                    pressed_keys[pygame.K_a]) * speed) % 800
     if jump:
-        rect.y -= jumpCount
+        rect_hero.y -= jumpCount
         if jumpCount > -jumpMax:
             jumpCount -= 1
         else:
             jump = False
-
+    all_sprites.update(rect_hero.x, rect_hero.y)
     screen.fill((0, 0, 255))
-    pygame.draw.rect(screen, (64, 64, 64), (0, 600, 1000, 800))
-    pygame.draw.circle(screen, (255, 0, 0), rect.center, 15)
+    pygame.draw.rect(screen, pygame.Color('brown'), (0, 435, 800, 600))
+    '''pygame.draw.rect(screen, (64, 64, 64), (0, 600, 1000, 800))
+    pygame.draw.circle(screen, (255, 0, 0), rect.center, 15)'''
+    all_sprites.draw(screen)
     pygame.display.flip()
